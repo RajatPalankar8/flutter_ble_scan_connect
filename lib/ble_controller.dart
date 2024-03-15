52f3ee8f54018cccc57992e7bd8cd3a44201ff3a
Rajat Palankar <rajatrrpalankar@gmail.com>
2024-03-15 17:12:10 +0530

    Flutter BLE - Scan & Connect to BLE

---
+++ lib/ble_controller.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class BleController extends GetxController{
+
+  FlutterBlue ble = FlutterBlue.instance;
+
+  Future scanDevices() async{
+    if(await Permission.bluetoothScan.request().isGranted){
+      if(await Permission.bluetoothConnect.request().isGranted){
+        ble.startScan(timeout: Duration(seconds: 15));
+
+        ble.stopScan();
+      }
+    }
+  }
+
+ Future<void> connectToDevice(BluetoothDevice device)async {
+
+    print(device);
+
+    await device?.connect(timeout: Duration(seconds: 15));
+
+    device?.state.listen((isConnected) {
+      if(isConnected == BluetoothDeviceState.connecting){
+        print("Device connecting to: ${device.name}");
+      }else if(isConnected == BluetoothDeviceState.connected){
+        print("Device connected: ${device.name}");
+      }else{
+        print("Device Disconnected");
+      }
+    });
+
+ }
+
+
+
+
+
+  Stream<List<ScanResult>> get scanResults => ble.scanResults;
+
+}
